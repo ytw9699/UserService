e6364bd168ca8d10691740a7005edcac3c25b264
home <ytw9699>
2023-04-25 01:03:32 +0900

    fix : 주석추가

--- UserService/src/main/java/com/example/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@EnableEurekaClient 이게 EnableDiscoveryClient를 구현해서 더 상품화 시켜둔것이긴하다. 어떤걸 사용해도됨
 public class UserServiceApplication {
 
     public static void main(String[] args) {
